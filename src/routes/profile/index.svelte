110429b81c6ae3d7ff2b4f1b248a76b93ae8bc5d
mathias <math8802@stud.kea.dk>
2022-06-25 15:09:29 +0200

    last modal fix

--- src/routes/profile/index.svelte
+++ src/routes/profile/index.svelte
@@ -26,12 +26,13 @@
 </script>
 
 <script>
-    import {patch} from "$lib/authApi";
+    import {patch, put} from "$lib/authApi";
     import {getNotificationsContext} from 'svelte-notifications';
     import {handleResponse} from "$lib/handleResponse";
     import EmployeeInfoBlock from "../../components/EmployeeInfoBlock.svelte";
     import UserInfoBlock from "../../components/UserInfoBlock.svelte";
     import {options} from "$lib/toaster";
+    import {goto} from "$app/navigation";
 
     const {addNotification} = getNotificationsContext();
 
@@ -62,7 +63,38 @@
         if (handled) {
             addNotification(options('Successfully updated your password!', 'success'));
         }
-    }
+    };
+
+    const edit = async () => {
+        employeeToUpdate.employee.email = employeeToUpdate.user.email;
+        const response = await put('/resources/employees/employee/own-details', employeeToUpdate);
+        const handled = handleResponse(response, addNotification);
+
+        if (handled) {
+            addNotification(options('Successfully updated your details!. Redirecting to profile!', 'success'));
+            await new Promise(r => setTimeout(r, 3000));
+
+            await goto('/profile');
+        }
+    };
+
+    const employeeToUpdate = {
+        employee: {
+            name: '',
+            email: '',
+            phone: '',
+            address: {
+                street: '',
+                city: '',
+                zip: '',
+                country: ''
+            }
+        },
+        user: {
+            username: '',
+            email: '',
+        }
+    };
 
 </script>
 
@@ -121,16 +153,74 @@
                     </div>
                 </div>
                 <div class="card-actions pt-5 justify-end">
-                    <a class="btn btn-ghost btn-sm btn-outline" rel="external" sveltekit:prefetch="" href="/profile/edit">Edit</a>
+                    <label for="{details.employee.id}" class="btn btn-ghost btn-sm btn-outline">Edit</label>
+                    <input type="checkbox" id="{details.employee.id}" class="modal-toggle" />
+                    <label for="{details.employee.id}" class="modal cursor-pointer">
+                        <label class="modal-box relative">
+                            <h2 class="card-title font-bold text-xl">Edit details</h2>
+                            <br/>
+                            <form on:submit|preventDefault={edit}>
+                                <div class="grid grid-cols-2 gap-3 w-full">
+                                    <div>
+                                        <h2 class="font-bold text-lg">Personal info</h2>
+                                        <div class="pt-5">
+                                            <h3 class="font-bold">Name</h3>
+                                            <input bind:value={employeeToUpdate.employee.name} type="text"
+                                                   placeholder="{details.employee.name}"
+                                                   class="input input-sm input-bordered max-w-sm"/>
+                                        </div>
+                                        <div class="pt-5">
+                                            <h3 class="font-bold">
+                                                Phone
+                                            </h3>
+                                            <input bind:value={employeeToUpdate.employee.phone} type="text"
+                                                   placeholder="{details.employee.phone}"
+                                                   class="input input-sm input-bordered max-w-sm"/>
+                                        </div>
+                                        <div class="pt-5">
+                                            <h3 class="font-bold">Street</h3>
+                                            <input bind:value={employeeToUpdate.employee.address.street} type="text"
+                                                   placeholder="{details.address.street}"
+                                                   class="input input-sm input-bordered max-w-sm"/>
+                                            <h3 class="font-bold">City</h3>
+                                            <input bind:value={employeeToUpdate.employee.address.zip} type="text"
+                                                   placeholder="{details.address.zip}"
+                                                   class="input input-sm input-bordered max-w-sm"/>
+                                            <h3 class="font-bold">Zip Code</h3>
+                                            <input bind:value={employeeToUpdate.employee.address.city} type="text"
+                                                   placeholder="{details.address.city}"
+                                                   class="input input-sm input-bordered max-w-sm"/>
+                                            <h3 class="font-bold">Country</h3>
+                                            <input bind:value={employeeToUpdate.employee.address.country} type="text"
+                                                   placeholder="{details.address.country}"
+                                                   class="input input-sm input-bordered max-w-sm"/>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <h2 class="text-lg font-bold">User info</h2>
+                                        <div class="pt-5">
+                                            <h3 class="font-bold">Username</h3>
+                                            <input bind:value={employeeToUpdate.user.username} type="text"
+                                                   placeholder="{details.user.username}"
+                                                   class="input input-sm input-bordered w-2/3 max-w-xs"/></div>
+                                        <div class="pt-5">
+                                            <h3 class="font-bold">Email</h3>
+                                            <input bind:value={employeeToUpdate.user.email} type="text"
+                                                   placeholder="{details.user.email}"
+                                                   class="input input-sm input-bordered w-2/3 max-w-xs"/></div>
+                                    </div>
+                                </div>
+                                <div class="card-actions pt-5 justify-end">
+                                    <div class="card-actions pt-5 justify-end">
+                                        <button class="btn btn-ghost btn-sm btn-outline" type="submit">Submit</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </label>
+                    </label>
                 </div>
             </div>
         </div>
     </div>
 </section>
 
-<style>
-    input, label {
-        display: inline-block;
-    }
-
-</style>
